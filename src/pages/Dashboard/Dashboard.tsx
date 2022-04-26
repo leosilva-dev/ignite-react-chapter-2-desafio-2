29734186c9915d8fe09032ba66c87e217f1c5c3a
Leonardo Silva <silvaleonardo.ti@gmail.com>
2022-04-26 15:27:32 -0300

    convert Dashboard to FC and TS

---
+++ src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,101 @@
+import React, { useEffect, useState } from "react";
+
+import { Header } from "../../components/Header/Header";
+import api from "../../services/api";
+import { Food } from "./../../components/Food/Food";
+import ModalAddFood from "../../components/ModalAddFood";
+import ModalEditFood from "../../components/ModalEditFood";
+import { FoodsContainer } from "./styles";
+import { IFood } from "../../interfaces/interfaces";
+
+export const Dashboard = () => {
+  const [foods, setFoods] = useState<IFood[]>([]);
+  const [editingFood, setEditingFood] = useState<IFood>({} as IFood);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [editModalOpen, setEditModalOpen] = useState(false);
+
+  const handleAddFood = async (food: IFood) => {
+    try {
+      const response = await api.post<IFood[]>("/foods", {
+        ...food,
+        available: true,
+      });
+
+      setFoods([...foods, ...response.data]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleUpdateFood = async (food: IFood) => {
+    try {
+      const foodUpdated = await api.put<IFood>(`/foods/${editingFood.id}`, {
+        ...editingFood,
+        ...food,
+      });
+
+      const foodsUpdated = foods.map((f) =>
+        f.id !== foodUpdated.data.id ? f : foodUpdated.data
+      );
+      setFoods(foodsUpdated);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleDeleteFood = async (id: string) => {
+    await api.delete(`/foods/${id}`);
+
+    const foodsFiltered = foods.filter((food) => food.id !== id);
+
+    setFoods(foodsFiltered);
+  };
+
+  const toggleModal = () => {
+    setModalOpen(!modalOpen);
+  };
+
+  const toggleEditModal = () => {
+    setEditModalOpen(!editModalOpen);
+  };
+
+  const handleEditFood = (food: IFood) => {
+    setEditingFood(food);
+    setEditModalOpen(true);
+  };
+
+  useEffect(() => {
+    api.get<IFood[]>("/foods").then((response) => {
+      setFoods([...response.data]);
+    });
+  }, []);
+
+  return (
+    <>
+      <Header openModal={toggleModal} />
+      <ModalAddFood
+        isOpen={modalOpen}
+        setIsOpen={toggleModal}
+        handleAddFood={handleAddFood}
+      />
+      <ModalEditFood
+        isOpen={editModalOpen}
+        setIsOpen={toggleEditModal}
+        editingFood={editingFood}
+        handleUpdateFood={handleUpdateFood}
+      />
+
+      <FoodsContainer data-testid="foods-list">
+        {foods &&
+          foods.map((food) => (
+            <Food
+              key={food.id}
+              food={food}
+              handleDelete={handleDeleteFood}
+              handleEditFood={handleEditFood}
+            />
+          ))}
+      </FoodsContainer>
+    </>
+  );
+};
